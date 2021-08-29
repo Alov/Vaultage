e9ad735732ceb6875789ee67a3f0c0c63e8f04b3
Ilya Shatalov <ilya@alov.me>
2021-08-29 18:36:54 +0300

    init commit

---
+++ vaultage/src/main/java/me/alov/vaultage/VaultageApplication.java
@@ -0,0 +1,13 @@
+package me.alov.vaultage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultageApplication.class, args);
+	}
+
+}
---
+++ vaultage/src/test/java/me/alov/vaultage/VaultageApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alov.vaultage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
